18f7f85cda8a80230f68614583248031409ed350
Wishk6 <guillaume.saurin@ynov.com>
2025-04-08 16:12:32 +0200

    Ajout de la configuration Jest, du fichier tsconfig.json et des tests pour les classes Calculatrice et FizzBuzz.

--- tests/jest.test.ts
+++ tests/jest.test.ts
@@ -1,13 +1,50 @@
+/* Class Calculatrice */
+class Calculatrice {
+    public addition(a: number, b: number): number {
+        return a + b;
+    }
+
+    public soustraction(a: number, b: number): number {
+        return a - b;
+    }
+
+    public multiplication(a: number, b: number): number {
+        return a * b;
+    }
+
+    public division(a: number, b: number): number {
+        if (b === 0) {
+            throw new Error('Division par zéro no no no');
+        }
+        return a / b;
+    }
+
+    public exponentiation(a: number, b: number): number {
+        return Math.pow(a, b);
+    }
+}
+
+/* Class FizzBuzz */
+class FizzBuzz {
+    public fizzBuzz(n: number): string {
+       switch (true) {
+        case n % 3 === 0 && n % 5 === 0:
+            return 'FizzBuzz';
+        case n % 3 === 0:
+            return 'Fizz';
+        case n % 5 === 0:
+            return 'Buzz';
+        default:
+            return n.toString();
+        }
+    }
+}
 
 
 /* Tests */
 describe('Tous les tests', () => {
     describe('Exercice 1 - Calculatrice', () => {
-        let calc;
-
-        beforeEach(() => {
-            calc = new Calculatrice();
-        });
+        const calc: Calculatrice = new Calculatrice();
 
         test('addition de deux nombres', () => {
             expect(calc.addition(2, 3)).toBe(5);
@@ -37,36 +74,13 @@ describe('Tous les tests', () => {
             expect(calc.exponentiation(2, 3)).toBe(8);
             expect(calc.exponentiation(5, 0)).toBe(1);
             expect(calc.exponentiation(2, -1)).toBe(0.5);
-            expect(calc.exponentiation(2, -2)).toBe(0.25);
-            expect(calc.exponentiation(9, 0.5)).toBe(3);
+        });
+    });
+
+    describe('Exercice 2 - FizzBuzz', () => {
+       const fizzBuzz: FizzBuzz = new FizzBuzz();
+       test('FizzBuzz', () => {
+        expect(fizzBuzz.fizzBuzz(2000000001)).toBe('Fizz');
         });
     });
 });
-
-/* Class Calculatrice */
-class Calculatrice {
-    addition(a, b) {
-        return a + b;
-    }
-
-    soustraction(a, b) {
-        return a - b;
-    }
-
-    multiplication(a, b) {
-        return a * b;
-    }
-
-    division(a, b) {
-        if (b === 0) {
-            throw new Error('Division par zéro no no no');
-        }
-        return a / b;
-    }
-
-    exponentiation(a, b) {
-        return Math.pow(a, b);
-    }
-}
-
-/* Class FizzBuzz */
